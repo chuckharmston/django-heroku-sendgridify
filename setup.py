3e2dc51bdf2983c02fa53e38de61101d0f4a7705
Chuck Harmston <chuck@chuckharmston.com>
2012-10-08 08:49:56 -0500

    Adds 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-heroku-sendgridify',
-    version='0.1',
+    version='0.1.1',
     description=(
         'Automatic email configuration for the Django/Heroku/SendGrid stack'
     ),
